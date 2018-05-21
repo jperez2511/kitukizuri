712b61a5ec25c26ca086e75abba5bd913cfe7f7f
Josue Perez <jperez.chek@gmail.com>
2018-05-20 21:22:17 -0600

    agregando a las rutas middleware de kitukizuri

--- src/resources/views/krud/index.blade.php
+++ src/resources/views/krud/index.blade.php
@@ -1,6 +1,7 @@
 @extends('layouts.app')
 
 @section('content')
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.0.13/css/all.css" integrity="sha384-DNOHZ68U8hZfKXOrtjWvjxusGo9WQnrNx2sqG0tfsghAvtVlRW3tvkXWZh58N9jp" crossorigin="anonymous">
     <link rel="stylesheet" type="text/css" href="/lib/datatables/css/dataTables.bootstrap.min.css"/>
     <style>
         div.dataTables_wrapper div.dataTables_filter{
