d90ad09826ef77df5e3f82e2818152ecd967c71c
Josue Perez <jperez.chek@gmail.com>
2018-05-20 21:33:21 -0600

    cambiando array a string

--- src/Http/routes.php
+++ src/Http/routes.php
@@ -4,7 +4,7 @@ Route::get('tururu', function () {
     dd('here kitukizuri');
 });
 
-Route::middleware(['auth', 'kitukizuri'])->group(['namespace' => 'Icebearsoft\Kitukizuri\Controllers'], function () {
+Route::middleware('auth', 'kitukizuri')->group(['namespace' => 'Icebearsoft\Kitukizuri\Controllers'], function () {
     Route::resource('roles', 'RolesController');
     Route::resource('rolpermisos', 'RolesPermisosController', ['only'=>['index', 'store']]);
 });
