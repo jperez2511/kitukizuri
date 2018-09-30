545ee34d0077630fcfdffb75a002dc7fee71f072
Josue Perez <jperez.chek@gmail.com>
2018-09-30 15:26:40 -0600

    agregando vista para el dashboard

---
+++ src/resources/views/kitukizuri/dashboard.blade.php
@@ -0,0 +1,7 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="col-md-12 text-center">
+    <h3>Bienvenido...</h3>
+</div>
+@stop
--- src/resources/views/krud/index.blade.php
+++ src/resources/views/krud/index.blade.php
@@ -49,7 +49,6 @@
             </div>
         </div>
     </div>
-    
 
     <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
