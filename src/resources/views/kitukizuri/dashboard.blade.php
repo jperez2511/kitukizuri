f0bc6fde2a77d48fdcfe1b37236092602216f1e2
Josue Perez <jperez.chek@gmail.com>
2019-06-30 22:19:03 -0600

    cambiando estilo visual del dashboard

--- src/resources/views/kitukizuri/dashboard.blade.php
+++ src/resources/views/kitukizuri/dashboard.blade.php
@@ -1,78 +1,53 @@
 @extends($layout)
 
 @section('content')
-<div class="col-lg-3 col-md-3">
-    <div class="card card-inverse card-primary">
+<div class="col-lg-3 col-md-6">
+    <div class="card">
         <div class="card-body">
-            <div class="d-flex">
-                <div class="m-r-20 align-self-center">
-                    <h1 class="text-white"><i class="zmdi zmdi-accounts zmdi-hc-fw"></i></h1>
-                </div>
-                <div>
-                    <h3 class="card-title">Total de usuarios</h3>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-12 text-center">
-                    <h1 class="font-light text-white">{{$usuarios}}</h1>
-                </div>
+            <div class="d-flex flex-row">
+                <div class="round round-lg align-self-center round-primary"><i class="zmdi zmdi-accounts zmdi-hc-fw"></i></div>
+                <div class="m-l-10 align-self-center">
+                    <h3 class="m-b-0 font-light">{{$usuarios}}</h3>
+                    <h5 class="text-muted m-b-0">Total Usuarios</h5></div>
             </div>
         </div>
     </div>
 </div>
-<div class="col-lg-3 col-md-3">
-    <div class="card card-inverse card-success">
+
+<div class="col-lg-3 col-md-6">
+    <div class="card">
         <div class="card-body">
-            <div class="d-flex">
-                <div class="m-r-20 align-self-center">
-                    <h1 class="text-white"><i class="zmdi zmdi-balance zmdi-hc-fw"></i></h1>
-                </div>
-                <div>
-                    <h3 class="card-title">Total de Empresas</h3>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-12 text-center">
-                    <h1 class="font-light text-white">{{$empresas}}</h1>
-                </div>
+            <div class="d-flex flex-row">
+                <div class="round round-lg align-self-center round-success"><i class="zmdi zmdi-balance zmdi-hc-fw"></i></div>
+                <div class="m-l-10 align-self-center">
+                    <h3 class="m-b-0 font-light">{{$empresas}}</h3>
+                    <h5 class="text-muted m-b-0">Total Empresas</h5></div>
             </div>
         </div>
     </div>
 </div>
-<div class="col-lg-3 col-md-3">
-    <div class="card card-inverse card-danger">
+
+<div class="col-lg-3 col-md-6">
+    <div class="card">
         <div class="card-body">
-            <div class="d-flex">
-                <div class="m-r-20 align-self-center">
-                    <h1 class="text-white"><i class="zmdi zmdi-apps zmdi-hc-fw"></i></h1>
-                </div>
-                <div>
-                    <h3 class="card-title">Total de Modulos</h3>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-12 text-center">
-                    <h1 class="font-light text-white">{{$modulos}}</h1>
-                </div>
+            <div class="d-flex flex-row">
+                <div class="round round-lg align-self-center round-danger"><i class="zmdi zmdi-apps zmdi-hc-fw"></i></div>
+                <div class="m-l-10 align-self-center">
+                    <h3 class="m-b-0 font-light">{{$modulos}}</h3>
+                    <h5 class="text-muted m-b-0">Total Modulos</h5></div>
             </div>
         </div>
     </div>
 </div>
-<div class="col-lg-3 col-md-3">
-    <div class="card card-inverse card-warning">
+
+<div class="col-lg-3 col-md-6">
+    <div class="card">
         <div class="card-body">
-            <div class="d-flex">
-                <div class="m-r-20 align-self-center">
-                    <h1 class="text-white"><i class="zmdi zmdi-traffic zmdi-hc-fw"></i></h1>
-                </div>
-                <div>
-                    <h3 class="card-title">Total de Roles</h3>
-                </div>
-            </div>
-            <div class="row">
-                <div class="col-12 text-center">
-                    <h1 class="font-light text-white">{{$roles}}</h1>
-                </div>
+            <div class="d-flex flex-row">
+                <div class="round round-lg align-self-center round-warning"><i class="zmdi zmdi-traffic zmdi-hc-fw"></i></div>
+                <div class="m-l-10 align-self-center">
+                    <h3 class="m-b-0 font-light">{{$roles}}</h3>
+                    <h5 class="text-muted m-b-0">Total Roles</h5></div>
             </div>
         </div>
     </div>
