52d5b025c0e85d4863b9be4b70ad174f8ccc35e0
Josue Perez <jperez.chek@gmail.com>
2021-08-30 17:52:12 -0600

    pificando variable opcional con php 8

--- src/Krud.php
+++ src/Krud.php
@@ -341,7 +341,7 @@ class Krud extends Controller
      *
      * @return void
      */
-    public function setWhere($column, $op='=', $column2)
+    public function setWhere($column, String $op='=', $column2)
     {
         array_push($this->wheres, [$column, $op, $column2]);
     }
