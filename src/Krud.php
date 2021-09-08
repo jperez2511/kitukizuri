9a0e6ed94fe7c5cde87da555381f699328f8e581
Josue Perez <jperez.chek@gmail.com>
2021-09-07 21:58:32 -0600

    quitando tificacion de string

--- src/Krud.php
+++ src/Krud.php
@@ -342,7 +342,7 @@ class Krud extends Controller
      *
      * @return void
      */
-    public function setWhere($column, String $op='=', String $column2)
+    public function setWhere($column, $op='=', $column2)
     {
         array_push($this->wheres, [$column, $op, $column2]);
     }
