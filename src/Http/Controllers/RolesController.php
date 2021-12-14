ae362ce84fc960185577e02b8ffbf475b0bf400b
Josue Perez <jperez.chek@gmail.com>
2021-12-14 14:06:49 -0600

    quitando validacion de roles por empresa

--- src/Http/Controllers/RolesController.php
+++ src/Http/Controllers/RolesController.php
@@ -24,13 +24,13 @@ class RolesController extends Krud
         $this->setCampo(['nombre'=>'Descripcion', 'campo'=>'descripcion']);
         $this->setBoton(['nombre'=>'Asignar Permisos', 'url'=>route('rolpermisos.index').'?id={id}', 'class'=>'outline-warning', 'icon'=>'mdi mdi-lock-open-variant-outline']);
         $this->setLayout('krud.layout');
+        
         $this->middleware(function ($request, $next) {
             if (!empty(Auth::user()->empresaid)) {
                 $usuarioRol = UsuarioRol::where('usuarioid', Auth::id())->get();
                 if ($usuarioRol->find(['rolid'=>1])->isEmpty()) {
                     $empresaid = Auth::user()->empresaid;
                     $this->setCampo(['nombre'=>'empresaid', 'campo'=>'empresaid', 'tipo' => 'hidden', 'value'=>$empresaid, 'show'=>false]);
-                    $this->setWhere('empresaid', '=', $empresaid);
                 }
             }
             return $next($request);
