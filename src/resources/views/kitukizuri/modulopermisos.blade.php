b965e06f50c6cc54b32b0d6b651ac59f8f96fa9c
Josue Perez - Linux <jperez.chek@gmail.com>
2021-12-27 15:31:49 -0600

    cambiando distribucion de columnas

--- src/resources/views/kitukizuri/modulopermisos.blade.php
+++ src/resources/views/kitukizuri/modulopermisos.blade.php
@@ -1,17 +1,18 @@
 @extends($layout)
 
 @section('content')
-
-    <div class="card">
-        <div class="card-header">
-            <h3>Asignar Permisos</h3>
-        </div>
-        <div class="card-body">
-            <span>Todos los usuarios con este rol tendrán asignados los permisos que se seleccionen.</span>
-            <div class="col-md-12 text-right">
-                <a href="javascript:void(0)" onclick="todos()" title="">Seleccionar Todos</a>
-                |
-                <a href="javascript:void(0)" onclick="ninguno()" title="">Ninguno</a>
+    <div class="col-12">
+        <div class="card">
+            <div class="card-header">
+                <h3>Asignar Permisos</h3>
+            </div>
+            <div class="card-body">
+                <span>Todos los usuarios con este rol tendrán asignados los permisos que se seleccionen.</span>
+                <div class="col-md-12 text-right">
+                    <a href="javascript:void(0)" onclick="todos()" title="">Seleccionar Todos</a>
+                    |
+                    <a href="javascript:void(0)" onclick="ninguno()" title="">Ninguno</a>
+                </div>
             </div>
         </div>
     </div>
@@ -20,19 +21,22 @@
         {{csrf_field()}}
         <div class="row">
             @foreach($modulos as $m)
-                <div class="card">
-                    <div class="card-header">
-                        <h4>{{$m->nombre}}</h4>
-                    </div>
-                    <div class="card-body">
-                        <div class="row">
-                            @foreach($m->modulopermiso()->get() as $mp)
-                                <?php $p = $mp->permisos()->first(); ?>
-                                <div class="col-3">
-                                    <input class="check" id="{{$mp->modulopermisoid}}" name="permisos[]" value="{{$mp->modulopermisoid}}" type="checkbox" {{in_array($mp->modulopermisoid, $rmp) ? 'checked' : ''}}>
-                                    <label for="{{$mp->modulopermisoid}}">{{$p->nombre}}</label>    
+                <div class="col-12">
+                    <div class="card">
+                        <div class="card-body">
+                            <div class="row">
+                                <div class="col-12">
+                                    <h4>{{$m->nombre}}</h4>
+                                    <hr>
                                 </div>
-                            @endforeach
+                                @foreach($m->modulopermiso()->get() as $mp)
+                                    <?php $p = $mp->permisos()->first(); ?>
+                                    <div class="col-3">
+                                        <input class="check" id="{{$mp->modulopermisoid}}" name="permisos[]" value="{{$mp->modulopermisoid}}" type="checkbox" {{in_array($mp->modulopermisoid, $rmp) ? 'checked' : ''}}>
+                                        <label for="{{$mp->modulopermisoid}}">{{$p->nombre}}</label>    
+                                    </div>
+                                @endforeach
+                            </div>
                         </div>
                     </div>
                 </div>
