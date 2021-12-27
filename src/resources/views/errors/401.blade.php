c9e516dcb63c8c0cc8718b62621aed28a46e4dcc
Josue Perez - Linux <jperez.chek@gmail.com>
2021-12-27 14:19:25 -0600

    prefesionalizando el mensaje de no tiene permisos

--- src/resources/views/errors/401.blade.php
+++ src/resources/views/errors/401.blade.php
@@ -1,11 +1,43 @@
-<html>
+<!doctype html>
+<html lang="en">
+
     <head>
-        <link href="{{ asset('css/style.css') }}" rel="stylesheet">
+        <meta charset="utf-8" />
+        <title>Kitu Kizuri</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <meta content="Panel de administración" name="description" />
+        <meta name="author" content="Ice Bear Soft">
+        <!-- App favicon -->
+        <link rel="shortcut icon" href="{{asset('kitukizuri/images/logo.svg')}}">
+
+        <!-- Bootstrap Css -->
+        <link href="{{asset('/kitukizuri/css/bootstrap-dark.min.css')}}" rel="stylesheet" type="text/css" />
+        <!-- Icons Css -->
+        <link href="{{asset('/kitukizuri/css/icons.min.css')}}" rel="stylesheet" type="text/css" />
+        <!-- App Css-->
+        <link href="{{asset('/kitukizuri/css/app-dark.min.css')}}" rel="stylesheet" type="text/css" />
+
     </head>
+
     <body>
-        <div class="container text-center">
-            <h1>No tienes permisos. <br>Buen intento.</h1>
-            <iframe src="https://giphy.com/embed/l0Exofw7Gx9c01VAI" width="439" height="480" frameBorder="0" class="giphy-embed" allowFullScreen></iframe>
+        <div class="home-btn d-none d-sm-block">
+            <a href="{{ url('') }}"><i class="mdi mdi-home-variant h2"></i></a>
         </div>
+        <div class="account-pages my-5 pt-5">
+            <div class="container text-center">
+                <h1>Vaya parece que no tienes permisos </h1>
+                <img src="{{asset('kitukizuri/libs/images/error.svg')}}" width="50%" alt="">
+            </div>
+        </div>
+        <!-- end Account pages -->
+
+        <!-- JAVASCRIPT -->
+        <script src="{{asset('kitukizuri/libs/jquery/jquery.min.js')}}"></script>
+        <script src="{{asset('kitukizuri/libs/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+        <script src="{{asset('kitukizuri/libs/metismenu/metisMenu.min.js')}}"></script>
+        <script src="{{asset('kitukizuri/libs/simplebar/simplebar.min.js')}}"></script>
+        <script src="{{asset('kitukizuri/libs/node-waves/waves.min.js')}}"></script>
+
+        <script src="{{asset('kitukizuri/js/app.js')}}"></script> 
     </body>
 </html>
