a3b95255976501fff8b160e5f1199cece64a8b0d
U-SERVIDORES\JEPerez <JEPerez@SPCNSSIIAP01.disagro.corp>
2022-10-10 14:35:20 -0600

    agregando componentes y validaciones para los parents

--- src/KitukizuriServiceProvider.php
+++ src/KitukizuriServiceProvider.php
@@ -50,7 +50,6 @@ class KitukizuriServiceProvider extends ServiceProvider
         });
     }
 
-
     /**
      * configureCommands
      *
--- src/resources/views/krud/components/textarea.blade.php
+++ src/resources/views/krud/components/textarea.blade.php
@@ -1,15 +1,20 @@
 @props([
     'columnClass' => 'col-md-6',
     'inputClass'  => '',
-    'label'
+    'label',
     'value' => ''
 ])
+
+@php
+    if(empty($inputClass)) {
+        $inputClass = 'form-control';
+    }
+@endphp
 
 <div class="{{$columnClass}}">
     <div class="form-group">
         <label>{{ $label }}</label>
         <textarea 
-            name="{{$nombre}}" 
             {!! $attributes->merge(['class' => $inputClass]) !!} 
             cols="30" 
             rows="10">{{$value}}</textarea>
