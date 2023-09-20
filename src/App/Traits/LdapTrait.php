398bff13abf12c1964501f74e36b0e1cc360a8dd
Josue Perez <jeperez@disagro.com>
2023-09-20 10:17:17 -0600

    generando stub para publicar configuracion de auth.php y cambiando la posicion de vendor publish

--- src/App/Traits/LdapTrait.php
+++ src/App/Traits/LdapTrait.php
@@ -7,7 +7,6 @@ trait LdapTrait
     protected function configLdap()
     {
         $this->composerInstall('directorytree/ldaprecord-laravel');
-        $this->artisanCommand('vendor:publish','--provider="LdapRecord\Laravel\LdapServiceProvider"'); 
         $this->addLdapEnv('.env');
         $this->addLdapEnv('.env.example');
         
@@ -16,31 +15,16 @@ trait LdapTrait
         $this->replaceInFile('Features::updateProfileInformation()', '// Features::updateProfileInformation()', base_path('config/fortify.php'));
         $this->replaceInFile('Features::updatePasswords()', '// Features::updatePasswords()', base_path('config/fortify.php'));
 
-        $this->editConfig('auth', [
-            'guards' => [
-                'web' => ['provider' => 'ldap']
-            ],
-            'providers' => [
-                'ldap' => [ 
-                    'driver' => 'ldap',
-                    'model' => LdapRecord\Models\ActiveDirectory\User::class,
-                    'database' => [
-                        'model' => App\Models\User::class,
-                        'sync_passwords' => true,
-                        'sync_attributes' => [
-                            'name' => 'cn',
-                            'email' => 'mail',
-                        ],
-                    ]
-                ]
-            ]
-        ]);
+        unlink(base_path('config/auth.php'));
+        copy(__DIR__ . '/../../stubs/Config/ldap.php', base_path('config/auth.php'));
 
         unlink(base_path('app/Providers/AuthServiceProvider.php'));
         copy(__DIR__ . '/../../stubs/Ldap/AuthServiceProvider.stub', base_path('app/Providers/AuthServiceProvider.php'));
         
         unlink(base_path('app/Models/User.php'));
         copy(__DIR__ . '/../../stubs/Ldap/User.stub', base_path('app/Models/User.php'));
+
+        $this->artisanCommand('vendor:publish','--provider="LdapRecord\Laravel\LdapServiceProvider"'); 
     }
 
     protected function addLdapEnv($file) 
