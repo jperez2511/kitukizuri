46ca73023e14721c2f8b9f5fd8e7e16d6a343088
Josue Perez <jperez.chek@gmail.com>
2023-09-25 18:04:40 -0600

    agregando validaciones en DB para no duplicar datos en el seeder inicial

--- src/App/Traits/MongoTrait.php
+++ src/App/Traits/MongoTrait.php
@@ -9,8 +9,9 @@ trait MongoTrait
     protected function configMongoDB()
     {
         $this->composerInstall('mongodb/laravel-mongodb:dev-master');
-
         $this->setDatabaseConfig();
+        $this->addMongoEnv('.env');
+        $this->addMongoEnv('.env.example');
     }
 
     private function setDatabaseConfig()
@@ -18,6 +19,7 @@ trait MongoTrait
         $path      = config_path('database.php');
         $contents  = File::get($path);
         $newConfig = <<<EOD
+                \n
                 'mongodb' => [
                     'driver'   => 'mongodb',
                     'host'     => env('DB_MONGO_HOST', '127.0.0.1'),
@@ -42,4 +44,29 @@ trait MongoTrait
 
         $this->info('Configuración de base de datos actualizada con éxito.');
     }
+
+    private function addMongoEnv($file)
+    {
+        $envPath = base_path($file);
+        $envContent = file_get_contents($envPath);
+
+        // Verifica si las configuraciones LDAP ya existen para evitar duplicados
+        if (!str_contains($envContent, 'DB_MONGO_CONNECTION')) {
+            $ldapConfig = "\n" .
+                "DB_MONGO_CONNECTION=mongodb\n" .
+                "DB_MONGO_HOST=mongo\n" .
+                "DB_MONGO_PORT=27017\n" .
+                "DB_MONGO_DATABASE=\n" .
+                "DB_MONGO_USERNAME=root\n" .
+                "DB_MONGO_PASSWORD=\n";
+
+            // Agrega las configuraciones al final del archivo .env
+            file_put_contents($envPath, $envContent . $ldapConfig);
+
+            $this->info('Configuraciones de MongoDB agregadas al '. $file);
+        } else {
+            $this->info('Las configuraciones de MongoDB ya existen en el '.$file);
+        }
+    }
+}
 }
--- src/database/seeders/InicialSeeder.php
+++ src/database/seeders/InicialSeeder.php
@@ -16,41 +16,51 @@ class InicialSeeder extends Seeder
 	{
 		DB::statement('SET FOREIGN_KEY_CHECKS=0');
 
-		DB::table('empresas')->insert([
-			'empresaid'     => 1,
-			'nombre'        => 'Empresa',
-			'telefono'      => '123456789',
-			'correo'        => 'admin@mail.com',
-			'nit'           => 'C/F',
-			'logo'          => '.',
-			'direccion'     => 'Guatemala',
-			'activo'        => true,
-		]);
+		$empresa = DB::table('empresas')->select('empresaid')->where('empresaid', 1)->first();
 
-		$this->call(EmpresamodulosSeeder::class);
+		if (empty($empresa)) {
+			DB::table('empresas')->insert([
+				'empresaid'     => 1,
+				'nombre'        => 'Empresa',
+				'telefono'      => '123456789',
+				'correo'        => 'admin@mail.com',
+				'nit'           => 'C/F',
+				'logo'          => '.',
+				'direccion'     => 'Guatemala',
+				'activo'        => true,
+			]);
 
-		DB::table('users')->insert([
-			'id'        => 1,
-			'empresaid' => 1,
-			'name'      => 'Administrator',
-			'email'     => 'admin@mail.com',
-			'password'  => '$2y$10$IFloXJUzFURcTFo.DQNZhO2FYGpWoLRZ9rgEf6cXiFK2Se8VZRyw2'
-		]);
-		
-		DB::table('roles')->insert([
-			['rolid' => 1,'nombre' => 'Super Usuario','descripcion' => 'Rol con todos los permisos',],
-			['rolid' => 2,'nombre' => 'Cliente','descripcion' => 'rol para todos clientes inciales',]
-		]);
+			$this->call(EmpresamodulosSeeder::class);
+		}
 
-		$this->call(RolModuloPermisosSeeder::class);
-		
-		DB::table('usuarioRol')->insert([
-			'usuariorolid' => 1,
-			'rolid'        => 1,
-			'usuarioid'    => 1
-		]);
+		$usuario = DB::table('users')->select('id')->where('id', 1)->first();
 
+		if(empty($usuario)) {
+			DB::table('users')->insert([
+				'id'        => 1,
+				'empresaid' => 1,
+				'name'      => 'Administrator',
+				'email'     => 'admin@mail.com',
+				'password'  => '$2y$10$IFloXJUzFURcTFo.DQNZhO2FYGpWoLRZ9rgEf6cXiFK2Se8VZRyw2'
+			]);
+		}
 
+		$roles = DB::table('roles')->select('rolid')->where('rolid', 1)->first();
+
+		if(empty($roles)){
+			DB::table('roles')->insert([
+				['rolid' => 1,'nombre' => 'Super Usuario','descripcion' => 'Rol con todos los permisos',],
+				['rolid' => 2,'nombre' => 'Cliente','descripcion' => 'rol para todos clientes inciales',]
+			]);
+
+			$this->call(RolModuloPermisosSeeder::class);
+
+			DB::table('usuarioRol')->insert([
+				'usuariorolid' => 1,
+				'rolid'        => 1,
+				'usuarioid'    => 1
+			]);
+		}
 
 		DB::statement('UPDATE users SET created_at=NOW(), updated_at=NOW()');
 		DB::statement('UPDATE roles SET created_at=NOW(), updated_at=NOW()');
