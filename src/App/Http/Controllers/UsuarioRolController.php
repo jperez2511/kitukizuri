786cf0f5e74b174d39da17ceda0187afcf87fd32
Josue Perez <jeperez@disagro.com>
2023-12-05 10:03:39 -0600

    validando si el valor parente esta encryptado o no

--- src/App/Http/Controllers/UsuarioRolController.php
+++ src/App/Http/Controllers/UsuarioRolController.php
@@ -5,6 +5,7 @@ namespace Icebearsoft\Kitukizuri\App\Http\Controllers;
 use Krud;
 use Auth;
 use Illuminate\Http\Request;
+use Illuminate\Contracts\Encryption\DecryptException;
 
 //=== Models
 use Icebearsoft\Kitukizuri\App\Models\{
@@ -25,13 +26,21 @@ class UsuarioRolController extends Krud
     {
         $collect = Rol::select('rolid as id', 'nombre as value')->get();
         
+        try {
+            // Intentar desencriptar y ver si no hay errores
+            $value = Crypt::decrypt($request->get('parent'));
+        } catch (DecryptException $e) {
+            // Si hay un error, probablemente el valor no está encriptado
+            $value = $request->get('parent');
+        }
+
         $this->setModel(new UsuarioRol);
         $this->setTitulo('Roles');
         $this->setCampo(['nombre'=>'Rol', 'campo'=>'r.nombre', 'edit' => false]);
         $this->setCampo(['nombre'=>'Descripcion', 'campo'=>'r.descripcion', 'edit'=>false]);
         $this->setCampo(['nombre'=>'Rol', 'campo'=>'usuarioRol.rolid', 'tipo'=>'combobox', 'collect'=>$collect, 'show'=>false]);
         $this->setJoin('roles as r', 'r.rolid', '=', 'usuarioRol.rolid');
-        $this->setWhere('usuarioid', '=', $request->get('parent'));
+        $this->setWhere('usuarioid', '=', $value);
         $this->setParents('usuarioid', 'parent', true);
         $this->setLayout('krud::layout');
     }
