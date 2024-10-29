201a22c43857c5d634b5ea3dc73f4d578b83f9d5
Josue Perez <jperez.chek@gmail.com>
2024-10-29 12:11:07 -0600

    agregando cambios en DT para la vista blade

--- src/resources/views/krud/index.blade.php
+++ src/resources/views/krud/index.blade.php
@@ -15,61 +15,13 @@
                     @foreach($columnas as $c)
                         <th>{{ $c }}</th>
                     @endforeach
-                    <th width="10%"></th>
+                    <th width="10%">{{ __('Actions') }}</th>
                 </thead>
                 <tbody>                        
                 </tbody>
             </table>
         </div>
     </div>
-    
-    @push('scripts')
-        <script>
-            document.addEventListener('DOMContentLoaded', function() {
-                var opt = {
-                    responsive: {
-                        details: true
-                    },
-                    buttons: ['copy', 'excel', 'csv', 'pdf']
-                };
-                var auto_responsive = $(this).data('auto-responsive'), has_export = (typeof (opt.buttons) !== 'undefined' && opt.buttons) ? true : false;
-                var btn = (has_export) ? '<"dt-export-buttons d-flex align-center"<"dt-export-title d-none d-md-inline-block">B>' : '', btn_cls = (has_export) ? ' with-export' : '';
-                var dom_normal = '<"row justify-between g-2' + btn_cls + '"<"col-7 col-sm-4 text-start"f><"col-5 col-sm-8 text-end"<"datatable-filter"<"d-flex justify-content-end g-2"' + btn + 'l>>>><"datatable-wrap my-3"t><"row align-items-center"<"col-7 col-sm-12 col-md-9"p><"col-5 col-sm-12 col-md-3 text-start text-md-end"i>>';
-                var dom_separate = '<"row justify-between g-2' + btn_cls + '"<"col-7 col-sm-4 text-start"f><"col-5 col-sm-8 text-end"<"datatable-filter"<"d-flex justify-content-end g-2"' + btn + 'l>>>><"my-3"t><"row align-items-center"<"col-7 col-sm-12 col-md-9"p><"col-5 col-sm-12 col-md-3 text-start text-md-end"i>>';
-                var dom = $(this).hasClass('is-separate') ? dom_separate : dom_normal;
-                $('#table1').DataTable({
-                    "ajax": {
-                        url:  "{{$ruta}}/0"+(String(window.location).includes('?') ? '?'+String(window.location).split('?')[1] : ''),
-                        type:  'GET',
-                    },
-                    'dom': dom,
-                    responsive: {
-                        details: true
-                    },
-                    buttons: ['copy', 'excel', 'csv', 'pdf'],
-                    autoWidth: false,
-                    language: {
-                        search: "",
-                        searchPlaceholder: "Type in to Search",
-                        lengthMenu: "<span class='d-none d-sm-inline-block'>Show</span><div class='form-control-select'> _MENU_ </div>",
-                        info: "_START_ -_END_ of _TOTAL_",
-                        infoEmpty: "0",
-                        infoFiltered: "( Total _MAX_  )",
-                        paginate: {
-                            "first": "First",
-                            "last": "Last",
-                            "next": "Next",
-                            "previous": "Prev"
-                        },
-                    }
-                })
-            });
-        </script>
-    @endpush
-
-</x-app-layout>
-
-@section('content')
 
     <div class="modal fade" id="myModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
         <div class="modal-dialog" role="document">
@@ -87,117 +39,112 @@
         </div>
     </div>
 
-@endsection
-@section('scripts')
-    <!-- Required datatable js -->
-    <script src="{{asset('kitukizuri/libs/datatables.net/js/jquery.dataTables.min.js')}}"></script>
-    <script src="{{asset('kitukizuri/libs/datatables.net-bs'.$vBootstrap.'/js/dataTables.bootstrap'.$vBootstrap.'.min.js')}}"></script>
-    <!-- Buttons examples -->
-    <script src="{{asset('kitukizuri/libs/datatables.net-buttons/js/dataTables.buttons.min.js')}}"></script>
-    <script src="{{asset('kitukizuri/libs/datatables.net-buttons-bs'.$vBootstrap.'/js/buttons.bootstrap'.$vBootstrap.'.min.js')}}"></script>
-    <script src="{{asset('kitukizuri/libs/jszip/jszip.min.js')}}"></script>
-    <script src="{{asset('kitukizuri/libs/pdfmake/build/pdfmake.min.js')}}"></script>
-    <script src="{{asset('kitukizuri/libs/pdfmake/build/vfs_fonts.js')}}"></script>
-    <script src="{{asset('kitukizuri/libs/datatables.net-buttons/js/buttons.html5.min.js')}}"></script>
-    <script src="{{asset('kitukizuri/libs/datatables.net-buttons/js/buttons.print.min.js')}}"></script>
-    <script src="{{asset('kitukizuri/libs/datatables.net-buttons/js/buttons.colVis.min.js')}}"></script>
-    <!-- Responsive examples -->
-    <script src="{{asset('kitukizuri/libs/datatables.net-responsive/js/dataTables.responsive.min.js')}}"></script>
-    <script src="{{asset('kitukizuri/libs/datatables.net-responsive-bs'.$vBootstrap.'/js/responsive.bootstrap'.$vBootstrap.'.min.js')}}"></script>
-    <script>
-        $('#table1').DataTable({
-            "language": {
-                "url": "{{asset('kitukizuri/libs/datatables.net/lang/es_es.json')}}"
-            },
-            "sortable": false,
-            "serverSide": true,
-            "processing": true,
-            "ajax": {
-                url:  "{{$ruta}}/0"+(String(window.location).includes('?') ? '?'+String(window.location).split('?')[1] : ''),
-		        type:  'GET',
-            },
-            "buttons":{
-                "buttons": [
-                    "copy",
-                    "excel",
-                    "pdf",
-                    "colvis",
-                    @if(in_array('create', $permisos))
-                        {
-                            text: 'Agregar',
-                            className: '{{$dtBtnAdd}}',
-                            action: function ( e, dt, node, config ) {
-                                location.assign('{{$ruta}}'+'/create'+(String(window.location).includes('?') ? '?'+String(window.location).split('?')[1] : ''));
-                            }
-                        }
-                    @endif
-                    @if(!empty($botonesDT))
-                        @foreach($botonesDT as $boton)
-                            {
-                                text: '{{ $boton['text'] }}',
-                                className: '{{ $boton['class'] }}',
-                                action: {!! $boton['action'] !!}
-                            }
-                        @endforeach
-                    @endif
-                ],
-                "dom": {
-                    button: {
-                        className: '{{$dtBtnLiner}}'
+    @push('scripts')
+        <script>
+           document.addEventListener('DOMContentLoaded', function() {
+                $('#table1').DataTable({
+                    language: {
+                        search           : "",
+                        searchPlaceholder: "{{ __('Search') }}",
+                        lengthMenu       : "<div class='form-control-select'> _MENU_ </div>",
+                        info             : "_START_ -_END_ of _TOTAL_",
+                        infoEmpty        : "0",
+                        infoFiltered     : "( Total _MAX_  )",
+                        paginate         : {
+                            "first": "{{ __('First') }}",
+                            "last": "{{ __('Last') }}",
+                            "next": "{{ __('Next') }}",
+                            "previous": "{{ __('Prev') }}"
+                        },
                     },
-                    buttonLiner: {
-                        tag: null
+                    sortable  : false,
+                    serverSide: true,
+                    processing: true,
+                    ajax      : {
+                        url : "{{$ruta}}/0"+(String(window.location).includes('?') ? '?'+String(window.location).split('?')[1] : ''),
+                        type: 'GET',
+                    },
+                    buttons: {
+                        buttons: [
+                            'copy', 'excel', 'pdf', 'colvis',
+                            @if(in_array('create', $permisos))
+                                {
+                                    className: 'btn-success buttons-create',
+                                    attr: {
+                                        title: '{{ __('Create') }}' // Tooltip en el botón
+                                    },
+                                    action: function ( e, dt, node, config ) {
+                                        window.location.href = '{{$ruta}}/create';
+                                    }
+                                },
+                            @endif
+                            @if(!empty($botonesDT))
+                                @foreach($botonesDT as $boton)
+                                    {
+                                        className: '{{ $boton['class'] }}',
+                                        attr: {
+                                            title: '{{ __($boton['text']) }}'
+                                        },
+                                        action: {!! $boton['action'] !!}
+                                    },
+                                @endforeach
+                            @endif   
+                        ],
+                    },
+                    sDom: '<"row justify-between g-2 with-export"<"col-7 col-sm-4 text-start"f><"col-5 col-sm-8 text-end"<"datatable-filter"<"d-flex justify-content-end g-2" <"dt-export-buttons d-flex align-center"<"dt-export-title d-none d-md-inline-block">B>l>>>><"datatable-wrap mb-2"t><"row align-items-center"<"col-7 col-sm-12 col-md-9"p><"col-5 col-sm-12 col-md-3 text-start text-md-end"i>>',
+                    initComplete: function() {
+                        $('.buttons-create').removeClass('btn-secondary');
                     }
-                }
-            },
-            "sDom": '<"row" <"col-sm-6" B><"col-sm-6" f>>rt <"row" <"col-sm-6" i><"col-sm-6" p>>'
-        })
-        function edit(id){
-            var url     = '{{$ruta}}';
-            var urlBase = String(window.location);
-            var id2     = null;
-            if(urlBase.includes('?')){
-                var result = urlBase.split('?');
-                url = result[0];
-                id2 = '?'+result[1];
-            }
-
-            window.location.assign(url+'/'+id+'/edit'+(id2 != null ? id2 : ''));
-        }
-        function destroy(id){
-            var url = String(window.location);
-            var id2 = null;
-            if(url.includes('?')){
-                var result = url.split('?');
-                url = result[0];
-                id2 = '?'+result[1];
-            }else{
-                id2 = '';
-            }
-
-            var confirmResult = confirm('¿Esta seguro de eliminar este elemento?');
-
-            if(confirmResult == true) {
-                $.post(url+'/'+id+id2,{_token:'{{csrf_token()}}', _method:'DELETE'}, function(data){
-                    if(data == 1) {
-                        window.location.reload();
-                    }else{
-                        alert(data);
-                    }
-                });
-            }
-        }
-        function opciones(id) {
-            var buttons = '';
-            var botones = {!! $botones !!}
-            console.log(botones)
-            $.each(botones, function(index, val) {
-                var url = val.url.replace('{id}', id);
-                buttons += '<div class="col-md-6"><a href="'+url+'" class="btn btn-'+val.class+' btn-block"><span class="'+val.icon+'"></span> '+val.nombre+'</a></div>'
+                })
             });
-            $('#modalContent').empty();
-            $('#modalContent').append(buttons);
-            $('#myModal').modal('show')
-        }
-    </script>
-@endsection
+            function edit(id){
+                var url     = '{{$ruta}}';
+                var urlBase = String(window.location);
+                var id2     = null;
+                if(urlBase.includes('?')){
+                    var result = urlBase.split('?');
+                    url = result[0];
+                    id2 = '?'+result[1];
+                }
+
+                window.location.assign(url+'/'+id+'/edit'+(id2 != null ? id2 : ''));
+            }
+            function destroy(id){
+                var url = String(window.location);
+                var id2 = null;
+                if(url.includes('?')){
+                    var result = url.split('?');
+                    url = result[0];
+                    id2 = '?'+result[1];
+                }else{
+                    id2 = '';
+                }
+
+                var confirmResult = confirm('¿Esta seguro de eliminar este elemento?');
+
+                if(confirmResult == true) {
+                    $.post(url+'/'+id+id2,{_token:'{{csrf_token()}}', _method:'DELETE'}, function(data){
+                        if(data == 1) {
+                            window.location.reload();
+                        }else{
+                            alert(data);
+                        }
+                    });
+                }
+            }
+            function opciones(id) {
+                var buttons = '';
+                var botones = {!! $botones !!}
+                console.log(botones)
+                $.each(botones, function(index, val) {
+                    var url = val.url.replace('{id}', id);
+                    buttons += '<div class="col-md-6"><a href="'+url+'" class="btn btn-'+val.class+' btn-block"><span class="'+val.icon+'"></span> '+val.nombre+'</a></div>'
+                });
+                $('#modalContent').empty();
+                $('#modalContent').append(buttons);
+                $('#myModal').modal('show')
+            }
+        </script>
+    @endpush
+
+</x-app-layout>
