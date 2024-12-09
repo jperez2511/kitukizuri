0458a3fb9ad582d1248128efdb15a4e10a41aed2
Josue Perez <jperez.chek@gmail.com>
2024-12-09 21:04:04 +0000

    corrigiendo nombre de variable

--- src/Krud.php
+++ src/Krud.php
@@ -230,10 +230,10 @@ class Krud extends Controller
                             $this->campos[$i]['value'] =  json_encode($value);
                         }
                     } else if ($this->campos[$i]['format'] == 'table') {
-                        $locationTableArray =  explode('.', $campos['campo']);
+                        $locationTableArray =  explode('.', $this->campos['campo']);
                         $args = [
                             $this->model->getKey(),
-                            $campos['idParent'],
+                            $this->campos['idParent'],
                             $locationTableArray[0],
                             $locationTableArray[1],
                         ];
