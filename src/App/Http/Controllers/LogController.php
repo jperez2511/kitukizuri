1e58e78bf3ffd93a62490db00cca9cc51fa80604
Josue Perez <jperez.chek@gmail.com>
2025-01-08 23:31:39 -0600

    generando loggin en base de datos y visor del mismo

--- src/App/Http/Controllers/LogController.php
+++ src/App/Http/Controllers/LogController.php
@@ -2,12 +2,34 @@
 
 namespace Icebearsoft\Kitukizuri\App\Http\Controllers;
 
-use App\Http\Controllers\Controller;
-
+use DB;
+use Krud;
+use Illuminate\Http\Request;
+use Log as Logger;
 use Icebearsoft\Kitukizuri\App\Models\Log;
-use Icebearsoft\Kitukizuri\App\Models\LaravelLogReader;
-class LogController extends Controller
+
+class LogController extends Krud
 {
+
+    public function __construct()
+    {
+        $this->setModel(new Log());
+        $channel = env('LOG_CHANNEL');
+
+        if($channel == 'database')
+        {
+            $this->setTitle('Logs');
+            $this->setField(['name' => 'Usuario', 'field' => 'u.email', 'htmlAttr' => ['disabled' => true]]);
+            $this->setField(['name' => 'URL', 'field' => DB::raw('SUBSTRING(url, 1, 50)'), 'campoReal' => 'url', 'htmlAttr' => ['disabled' => true]]);
+            $this->setField(['name' => 'Métodos', 'field' => 'method']);
+            $this->setField(['name' => 'Nivel', 'field' => 'level']);
+            $this->setField(['name' => 'Mensaje', 'field' => 'message']);
+            $this->leftJoin('users as u', 'u.id', '=', 'logs.id_user');
+            $this->orderBy('id_log', 'desc');
+            $this->setButton(['nombre' => 'Ver', 'url' => 'logs/{id}', 'class' => 'btn btn-outline-primary', 'icon' => 'fa fa-eye']);
+        }
+    }
+
     public function index()
     {
         $channel = env('LOG_CHANNEL');
@@ -17,17 +39,16 @@ class LogController extends Controller
             $file = storage_path('logs/laravel.log');
             return response()->download($file, 'laravel.log', ['Content-Type' => 'text/plain']);
         } else if ($channel == 'database') {
-            $log = Log::orderBy('id_log', 'desc')->get();
+            return parent::index();
         }
+    }
 
-        
+    public function show($id, Request $request)
+    {
+        if ($request->ajax() == false) {
+            return  $this->edit($id, $request);
+        } 
 
-        return view('kitukizuri::log', [
-            'logs'   => $log,
-            'titulo' => 'Logs',
-            'kmenu'  => true,
-            'layout' => 'krud::layout',
-            'dash'   => true
-        ]);
+        return parent::show($id, $request);
     }
 }
