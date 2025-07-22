648adc198d64c21983e798b09261c9769e3ee112
Josue Perez <jperez.chek@gmail.com>
2025-07-22 12:33:23 -0600

    configurando vite

--- src/App/Traits/ReactTrait.php
+++ src/App/Traits/ReactTrait.php
@@ -40,19 +40,7 @@ trait ReactTrait
             $this->info('Las configuración para Vue ya existen en el app.js');
         }
     }
-
-    protected function addViteConfig()
-    {
-        unlink(base_path('vite.config.js'));
-        copy(__DIR__ . '/../../stubs/vite.config.js', base_path('vite.config.js'));
-
-        $this->replaceInFile('import vue from \'@vitejs/plugin-vue\';', 'import react from \'@vitejs/plugin-react\';', base_path('vite.config.js'));
-
-
-
-        $this->info('La configuración de React en Vite fue agregada exitosamente');
-    }
- }
+}
 
 
 
